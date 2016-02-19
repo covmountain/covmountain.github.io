aadbfe2745ee03d45930d56838ae99369112b021
Ed Prince <edward_prince@hotmail.com>
2016-02-19 00:52:50 +0000

    team grid fixed

--- _site/js/equipment.js
+++ _site/js/equipment.js
@@ -10,30 +10,41 @@ function main() {
   currentDisplay = $(".dynamic-display");
 
   climbingButton.click(function() {
-    $("#other-display").hide();
-    $("#winter-display").hide();
-    $("#hiking-display").hide();
-    $("#climbing-display").show();
+    display_climbing();
   });
   hikingButton.click(function() {
-    $("#other-display").hide();
-    $("#climbing-display").hide()
-    $("#winter-display").hide();
-    $("#hiking-display").show();
+    display_hiking();
   });
   winterButton.click(function() {
-    $("#hiking-display").hide();
-    $("#climbing-display").hide()
-    $("#other-display").hide();
-    $("#winter-display").show();
+    display_winter();
   });
   otherButton.click(function() {
-    $("#hiking-display").hide();
-    $("#climbing-display").hide();
-    $("#winter-display").hide();
-    $("#other-display").show();
+    display_other();
   });
-
 }
 
+function display_climbing() {
+  $("#other-display").hide();
+  $("#winter-display").hide();
+  $("#hiking-display").hide();
+  $("#climbing-display").show();
+}
+function display_winter() {
+  $("#hiking-display").hide();
+  $("#climbing-display").hide()
+  $("#other-display").hide();
+  $("#winter-display").show();
+}
+function display_hiking() {
+  $("#other-display").hide();
+  $("#climbing-display").hide()
+  $("#winter-display").hide();
+  $("#hiking-display").show();
+}
 
+function display_other() {
+  $("#hiking-display").hide();
+  $("#climbing-display").hide();
+  $("#winter-display").hide();
+  $("#other-display").show();
+}
