74f5c2c5a3e1647774e2c3fce03dbaf8dcab5adb
Ed Prince <edward_prince@hotmail.com>
2016-05-12 02:56:34 +0100

    calendar added

--- _site/gc/jq-fc.js
+++ _site/gc/jq-fc.js
@@ -16,10 +16,6 @@ $(document).ready(function() {
     eventSources: [
     {
       url: 'gc/events.php',
-    },
-    {  
-      url: 'gc/sports.php',
-      color: '#257E4A'
     }
     ],
     allDayDefaul: true,
@@ -82,68 +78,6 @@ $(document).ready(function() {
 function unixTimeStamp(x) {
   return Date.parse(x); 
 }
-/*
-function displayList(category, data) {
-    console.log("Display function running");
-    var upcoming = document.getElementById(category);
-    var maxResults = 3;
-    var upcomingEvents = [];
-    //Make i the index of the first result that matches or succeeds the current date
-    data.forEach(function(i) {
-      console.log(i);
-      console.log((unixTimeStamp(i.start)), " - ", (unixTimeStamp(new Date().toJSON())));
-      if ((unixTimeStamp(i.start)) >= (Date.parse(new Date().toJSON()))) {
-        upcomingEvents.push(i);
-      }
-    });
-
-    console.log("UPCOMING EVENTS: ", upcomingEvents);
-
-    for (var i = 0; i < maxResults; i++) {
-      var li = document.createElement('li');
-      var h3 = document.createElement('h3');
-
-      h3.appendChild(document.createTextNode(upcomingEvents[i].title));
-      h3.setAttribute('id', 'list-title');
-      li.appendChild(h3);
-      upcoming.appendChild(li);
-
-      var date = upcomingEvents[i].start;
-      var listEndDate = upcomingEvents[i].end;
-      console.log(listEndDate, "end date");
-      
-
-      if (!date) {
-        date = upcomingEvents.start;
-      }
-      date = moment(date).format('MMMM Do YYYY, h:mm:ss a');
-
-      var time;
-      time = date.split(',')[1];
-      date = date.split(',')[0];
-
-      if (listEndDate.split('T')[1]) {
-        time = time + " - " + moment(listEndDate).format('h:mm a');
-      }
-
-      if (i === maxResults - 1) {
-        li.setAttribute('id', 'list-bottom');
-      } else {
-        li.style.borderBottom = 'dotted 1px #DDD';
-      }
-      var listDate = document.createElement('p');
-      var listTime = document.createElement('p');
-      listTime.setAttribute('id', 'list-time');
-      listTime.appendChild(document.createTextNode(time));
-      listDate.setAttribute('id', 'list-date');
-      listDate.appendChild(document.createTextNode(date));
-      li.appendChild(listDate);
-      li.appendChild(listTime);
-    }
-    console.log("JQ FINISHED");
-}
-*/
-
 
 function closeModal(e) {
     e.stopPropagation(); 
--- _site/gc/sports.php
+++
@@ -1,4 +0,0 @@
-<?php 
-  $calendarId = 'ihi8tl0bikominua641vrnmqcs@group.calendar.google.com';
-  require 'common.php';
-?>
