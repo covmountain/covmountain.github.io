66e3b4c8c76a97776b0618b7fcad671858f8d990
edprince <edward_prince@hotmail.com>
2016-11-10 14:55:34 +0000

    carousel

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ window.onload = function() {
   var x = 0;
   var banner = document.getElementById('banner');
   window.setInterval(function() { 
-    if x = images.length {
+    if (x == (images.length - 1)) {
       x = 0;
     }
     banner.style.background-image='url("images/light-bl.svg"), url("images/light-br.svg"), url("images/overlay.png"), url("../images/' + images[x] + '.jpg")';
