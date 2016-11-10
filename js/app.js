bb68b3e65a46d03cd4a7b2a1e531caeaa4b4f708
edprince <edward_prince@hotmail.com>
2016-11-10 14:53:58 +0000

    carousel

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  var images = [
+    'banner1.jpg',
+    'banner2.jpg',
+    'banner3.jpg',
+    'banner4.jpg'
+  ];
+  var x = 0;
+  var banner = document.getElementById('banner');
+  window.setInterval(function() { 
+    if x = images.length {
+      x = 0;
+    }
+    banner.style.background-image='url("images/light-bl.svg"), url("images/light-br.svg"), url("images/overlay.png"), url("../images/' + images[x] + '.jpg")';
+
+  }, 5000);
+}
